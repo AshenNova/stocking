3c4ae7e3e5197ab144c81392c7bc8d6a716c4ea4
ashennova <kennerve14@gmail.com>
2022-02-11 15:42:04 +0800

    Final submit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,18 @@
 class HomeController < ApplicationController
   def index
-    @api = StockQuote::Stock.new(api_key: 'pk_2855e131a459418b87bd137228b9f0fe')
-    if params[:ticker] == ""
-      @nothing = "Hey! You forgot to enter a symbol"
-    elsif params[:ticker]
-      @stock = StockQuote::Stock.quote(params[:ticker])
-        if !@stock
-          @error = "Hey! That Stock Symbol doesnt exist!"
-      end 
-    end
+    begin
+      @api = StockQuote::Stock.new(api_key: 'pk_2855e131a459418b87bd137228b9f0fe')
+      if params[:ticker] == ""
+        @nothing = "Hey! You forgot to enter a symbol"
+      elsif params[:ticker]
+        @stock = StockQuote::Stock.quote(params[:ticker])
+          if !@stock
+            @error = "Hey! That Stock Symbol doesnt exist!"
+        end 
+      end
+    rescue
+      @error = "Hey! That Stock Symbol doesnt exist!"
+    end 
   end
 
   def about
